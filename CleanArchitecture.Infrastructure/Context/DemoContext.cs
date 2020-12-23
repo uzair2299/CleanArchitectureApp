e206f98db8a2473aa9b0f5bc2e1fac8ea0ac53a6
uzair <uzairanwar2299@gmail.com>
2020-12-24 00:40:05 +0500

    Add project files.

---
+++ CleanArchitecture.Infrastructure/Context/DemoContext.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitectureDomain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Infrastructure.Context
+{
+   public class DemoContext:DbContext
+    {
+         public DemoContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
