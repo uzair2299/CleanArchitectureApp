b665f24ea7b7c76464a02dfe1e4babfc2865dfe6
uzair <uzairanwar2299@gmail.com>
2020-12-24 01:08:32 +0500

    Changes

---
+++ CleanArchitecture.Core/Interfaces/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using CleanArchitecture.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Core.Interfaces
+{
+    interface IBookService
+    {
+        BookViewModel GetBooks();
+    }
+}
---
+++ CleanArchitecture.Core/ViewModels/BookViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CleanArchitectureDomain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Core.ViewModels
+{
+    public class BookViewModel
+    {
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
